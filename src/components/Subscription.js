d50622a55c039d8e1921ff4b07f27d239e1b317e
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-05-28 19:52:56 -0400

    Update colors

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -6,9 +6,9 @@ export default function Subscription() {
         <section className="subscription">
             <h2>Monthly Subscription</h2>
             <div className="subscription--price">
-                &dollar;29 <span>per month</span>
+                $29 <span>per month</span>
             </div>
-            <p>Full access for less than &dollar;1 a day</p>
+            <p>Full access for less than $1 a day</p>
             <Signuplink />
         </section>
     )
