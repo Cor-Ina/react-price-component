3d8a594d12816f4a467daa3d9edbc240471e8b0d
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-05-28 21:24:31 -0400

    Add title to subscription link

--- src/components/Signuplink.js
+++ src/components/Signuplink.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Signuplink() {
     return (
         <div className="signuplink">
-            <a href="https://www.frontendmentor.io/profile/Cor-Ina">Sign Up</a>
+            <a href="https://www.frontendmentor.io/profile/Cor-Ina" title="Visit Corina's Frontend Mentor page">Sign Up</a>
         </div>
         
     )
